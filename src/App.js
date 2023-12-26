b980fafcf287833ccb4600ddb05e62be71eab580
ssk <s99ruy@gmail.com>
2023-12-26 14:35:15 +0100

    New route files, react-router setup in app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Registration from './pages/Registration';
+import ItemPage from './pages/ItemPage';
+import NotFound from './pages/404';
 import ThemeProvider from './components/styles/ThemeProvider';
 import GlobalStyle from './components/styles/Global'
 
-function App() {
-  return (
-    <ThemeProvider>
-	    <>
-	    <GlobalStyle />
-		    <div>
-		        <h1>Themed React App</h1>
-		    </div>
-		</>
-    </ThemeProvider>
-  );
-}
+
+	function App() {
+		return (
+			<Router>
+		  	<ThemeProvider>
+			<GlobalStyle />
+			<Switch>
+			  <Route path="/" exact component={Home} />
+			  <Route path="/login" component={Login} />
+			  <Route path="/registration" component={Registration} />
+			  <Route path="/items/:itemId" component={ItemPage} />
+			  <Route path="*" component={NotFound} />
+			</Switch>
+			</ThemeProvider>
+			</Router>
+		);
+	  }
 
 export default App;
