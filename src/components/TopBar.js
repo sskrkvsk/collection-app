37b6c4db9111b7b5f745df70e2cc2c144a466aa0
ssk <s99ruy@gmail.com>
2024-01-04 05:37:17 +0100

    fixed random btn in similar manner with a history hook

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { TopBartyle } from './styles/TopBar.styled';
 
 const TopBar = ({ sorting, setSorting, toggleGrid, tableName, sortingFunction, tableData }) => {
+  const history = useHistory();
   // Show btn
   function handleClick() {
     sorting ? setSorting(false) : setSorting(true);
@@ -12,19 +13,18 @@ const TopBar = ({ sorting, setSorting, toggleGrid, tableName, sortingFunction, t
   const newTableData = tableData.map((table) => {
     return table.title
   })
-  const [randomItem, setRandomItem] = useState('');
   const handleRandom = () => {
     const randomValue = Math.floor(Math.random() * newTableData.length);
     const trimmedTitle = encodeURIComponent(String(newTableData[randomValue]).replace(/\s+/g, '-'));
 
-    setRandomItem(trimmedTitle);
+    history.push(`/${tableName}/${trimmedTitle}`);
   }
 
   return (
     <TopBartyle>
         <li><Link to="/">Home</Link></li>
         <li><Link to={`/additem?category=${tableName}`}>Add item</Link></li>
-        <li onMouseEnter={handleRandom}><Link to={`/${tableName}/${randomItem}`}>Random</Link></li>
+        <li onClick={handleRandom}>Random</li>
         <li><Link to="/some-route">User</Link></li>
         <div>
           <button onClick={toggleGrid}>Grid</button>
