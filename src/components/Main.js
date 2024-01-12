36282f1222fa63098f3b8e1c3bc74948fc947497
ssks <62514297+sskrkvsk@users.noreply.github.com>
2024-01-12 14:41:25 +0100

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -12,7 +12,7 @@ const Main = () => {
   const [deleteStatus, setDeleteStatus] = useState(false);
 
   useEffect(() => {
-    axios.get(`https://ep-quiet-frog-84894977-pooler.us-east-1.postgres.vercel-storage.com/api/getTableNames`)
+    axios.get(`https://ep-quiet-frog-84894977-pooler.us-east-1.postgres.vercel-storage.com/getTableNames`)
     .then(response => {
       
       setTableNames(response.data.finalArray);
